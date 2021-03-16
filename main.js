9669f9ea015b83b7ca4336d233f8679d9f895b49
EC2 Default User <ec2-user@ip-172-31-9-103.ap-northeast-1.compute.internal>
2021-03-16 02:52:15 +0000

    「0」を押した後、数字を入力すると、前0表示になるバグ修正

--- main.js
+++ main.js
@@ -1,11 +1,47 @@
 'use strict';
 
+let count = 0;
+
 function update(_v) {
   document.querySelector('input').value = _v;
+  count = 0;
 }
 
 function append(_v) {
+
+  const suppressZero = str => {
+    let idx = 0;
+    while (str.charAt(idx) === '0') idx++;
+    return str.slice(idx);
+  };
+
   document.querySelector('input').value += _v;
+
+  if (count > 0 && document.querySelector('input').value !== '.') {
+    // if (document.querySelector('input').value !== '.') {
+    document.querySelector('input').value = suppressZero(document.querySelector('input').value);
+    // }
+  }
+  count++;
+}
+
+function symbol(_v) {
+  let lastStr = document.querySelector('input').value[document.querySelector('input').value.length - 1]
+  if (document.querySelector('input').value.slice(0) === '.') {
+    document.querySelector('input').value = '0' + document.querySelector('input').value;
+  }
+  // console.log(lastStr);
+
+  if (lastStr == "/" || lastStr == "*" || lastStr == "+" || lastStr == "-") {
+
+    document.querySelector('input').value = document.querySelector('input').value.slice(0, -1)
+
+    document.querySelector('input').value = document.querySelector('input').value + _v
+  }
+  else {
+    document.querySelector('input').value = document.querySelector('input').value + _v
+  }
+
 }
 
 function calc() {
@@ -13,6 +49,7 @@ function calc() {
   try {
     const f = new Function('return ' + v);
     update(f().toString());
+    count = 0;
   }
   catch (_error) {
     update(_error);
